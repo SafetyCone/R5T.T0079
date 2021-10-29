d0aaecb4b5cfad2863fbc7bfd22765280eb8703a
davidcoats <david@getsafetycone.com>
2021-10-29 12:47:37 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0079.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0079.Construction
+{
+    /// <summary>
+    /// Construction project for IServiceCollectionBuilder and IServiceProviderBuilder interface definitions and default implementations..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0079/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0079
+{
+    /// <summary>
+    /// IServiceCollectionBuilder and IServiceProviderBuilder interface definitions and default implementations.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
